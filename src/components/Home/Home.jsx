5a408e53bc264fea7019fdb05e27f7a6876d0dbd
tarunsinghal733 <20BCS3552cuchd.in>
2023-11-21 17:04:10 +0530

    ebn

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import "../Home/Home.css"
+import CarImage from '../../assets/12.jpg';
+
 
 const Home = () => {
   return (
-    <div>
-
+    <div className='home'>
+      <div className='homeimg'>
+        <img src={CarImage} alt='tour' />
+      </div>
+      <div className='hometext'>
+        <h1>Your Journey Your Story</h1>
+        <p>Choose Your Favourite Destination</p>
+        <a className='show' href='#'>Travel Plan</a>
+      </div>
     </div>
   )
 }
